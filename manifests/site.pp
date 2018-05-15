5f56f809739f35f2b4c7f580b181232e47b25f2e
alondiana <39295250+alondiana@users.noreply.github.com>
2018-05-15 15:14:59 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,5 @@
 node default {
-  file {'/root/README':
-    ensure => file,
-    content => 'This is readme',
-    }    
+  }
+node ip-172-31-30-83.eu-central-1.compute.internal   {    
+  include role:master_server
 }
